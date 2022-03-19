e7a75ac34503a151f111296aea8492e26f345029
多喜 吕 <lduoxi@126.com>
2022-03-19 18:45:11 +0800

    更新

--- WindowsFormsApp3/Form2.cs
+++ WindowsFormsApp3/Form2.cs
@@ -24,5 +24,10 @@ namespace WindowsFormsApp3
             f1.ShowDialog();
             this.Close();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
